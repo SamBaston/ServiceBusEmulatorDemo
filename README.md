30a0fc33bd30fb5990d583c3b344385d73c1641c
SamBaston <sam.baston@icloud.com>
2025-05-21 09:27:07 +0100

    removed the dockerfile as it isn't needed for this demo. added the docker compose, service bus config and actual program files.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ServiceBusEmulatorDemo
 Demo code for a service bus emulator
+
+# Service Bus Emulator
+To get this demo running, Docker Engine and WSL will need to be installed first. 
+
+1. Open up Docker Desktop which will start Docker Engine automatically.
+2. Open up a terminal and navigate to the ServiceBusEmulator, where the docker-compose file is located.
+3. Run the following command to run the emulator in detached mode:
+    "docker-compose up -d"
+4. Once the terminal shows the containers running, go into Docker Desktop > Containers > microsoft-azure-servicebus-emulator. There should now be servicebus-emulator and sqledge.
+
+To stop the emulator, use the following command:
+    "docker-compose down"
--- src/ServiceBusEmulator/Program.cs
+++ src/ServiceBusEmulator/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+
+var serviceBusClient = new ServiceBusClient("Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;");
+
+var sender = serviceBusClient.CreateSender("Receiver");
+
+var message = new DemoMessage()
+{
+    Id = Guid.NewGuid(),
+    Message = "Test Message"
+};
+
+var body = new BinaryData(JsonSerializer.Serialize(message));
+var serviceBusMessage = new ServiceBusMessage(body);
+
+await sender.SendMessageAsync(serviceBusMessage);
+Console.WriteLine("Message sent");
+
+var processor = serviceBusClient.CreateProcessor("Receiver", "Subscription");
+processor.ProcessMessageAsync += (args) => {
+
+    var receivedMessage = JsonSerializer.Deserialize<DemoMessage>(args.Message.Body);
+    Console.WriteLine($"Message received: {receivedMessage.Message}");
+    return Task.CompletedTask;
+};
+processor.ProcessErrorAsync += (args) =>
+{
+    Console.WriteLine($"Error occurred: {args.Exception.Message}");
+    return Task.CompletedTask;
+};
+
+await processor.StartProcessingAsync();
+Console.WriteLine("Message processing started");
+
+while (true)
+{
+    await Task.Delay(1000);
+}
+
+
+public class DemoMessage
+{
+    public Guid Id { get; set; }
+    public string Message { get; set; }
+}
